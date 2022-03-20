6d0a0cd8498e3c808bd7a23a80ee5d55ee670653
toke8677 <toke8677@gmail.com>
2022-03-20 13:24:47 +0200

    fix emoji

--- scripts/bar_battery.sh
+++ scripts/bar_battery.sh
@@ -3,34 +3,35 @@ battery_capacity="$(cat /sys/class/power_supply/$battery/capacity)"
 battery_status="$(cat /sys/class/power_supply/$battery/status)"
 model="$(cat /sys/class/power_supply/$battery/model_name | awk '{print $1" "$2}' | xargs)"
 
+emoji=""
 if [ "$battery_status" != "Charging" ]; then
     if [ "$battery_capacity" -lt 10 ]; then
-        printf ""
+        emoji=""
     elif [ "$battery_capacity" -lt 20 ]; then
-        printf ""
+        emoji=""
     elif [ "$battery_capacity" -lt 30 ]; then
-        printf ""
+        emoji=""
     elif [ "$battery_capacity" -lt 40 ]; then
-        printf ""
+        emoji=""
     elif [ "$battery_capacity" -lt 50 ]; then
-        printf ""
+        emoji=""
     elif [ "$battery_capacity" -lt 60 ]; then
-        printf ""
+        emoji=""
     elif [ "$battery_capacity" -lt 70 ]; then
-        printf ""
+        emoji=""
     elif [ "$battery_capacity" -lt 80 ]; then
-        printf ""
+        emoji=""
     elif [ "$battery_capacity" -lt 90 ]; then
-        printf ""
+        emoji=""
     elif [ "$battery_capacity" -lt 100 ]; then
-        printf ""
+        emoji=""
     fi
-else 
-    printf ""
+else
+    emoji=""
 fi
 
 if [ "$battery_status" != "Unknown" ]; then
-    echo " $model: $battery_status ($battery_capacity%)"
+    echo "$emoji $model: $battery_status ($battery_capacity%)"
 else
-    echo " $model: $battery_capacity%"
+    echo "$emoji $model: $battery_capacity%"
 fi
