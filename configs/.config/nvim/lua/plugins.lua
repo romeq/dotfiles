9682b68c11e93ee81d6085e15726204da84f79c2
romeq <me@romeq.fi>
2022-04-24 02:47:12 +0300

    add crates.nvim

--- configs/.config/nvim/lua/plugins.lua
+++ configs/.config/nvim/lua/plugins.lua
@@ -14,5 +14,11 @@ return require('packer').startup(function()
     use {'lajp/testaustime-nvim', run = 'cabal install --overwrite-policy=always' }
     use 'glepnir/dashboard-nvim'
     use 'liuchengxu/vim-clap'
+    use {
+        'saecki/crates.nvim',
+        tag = 'v0.1.0',
+        requires = { 'nvim-lua/plenary.nvim' },
+        config = require('crates_config')
+    }
 end)
 
